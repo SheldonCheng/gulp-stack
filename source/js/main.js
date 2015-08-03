f35b4982978cdeb6f053336776431aee98963a8b
Sheldon Cheng <sheldonscyd@gmail.com>
2015-08-03 22:47:07 +0800

    Add basic tabs component

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function(){
+
+  // Selectize component
   $('.js-select').selectize();
+
+  // Tabs component
+  $('.js-tabs').on('click', '.tabs-link a',function(e){
+    e.preventDefault();
+    $(this).parent('li').addClass('active').siblings('.active').removeClass('active');
+    var target = $(this).attr('href');
+    $(target).addClass('active').siblings('.active').removeClass('active');
+  });
+
 });
